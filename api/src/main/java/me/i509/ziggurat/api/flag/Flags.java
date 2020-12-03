5b6a74532f45c2cf84c933203902360812a502ec
i509VCB <git@i509.me>
2020-12-02 21:30:13 -0600

    Some more flags and list ideas

--- api/src/main/java/me/i509/ziggurat/api/flag/Flags.java
+++ api/src/main/java/me/i509/ziggurat/api/flag/Flags.java
@@ -1,43 +1,71 @@
 package me.i509.ziggurat.api.flag;
 
-import java.util.UUID;
+import static me.i509.ziggurat.internal.ZigguratImplementation.id;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.util.registry.Registry;
+import java.util.UUID;
 
 import me.i509.ziggurat.api.region.GlobalRegion;
 import me.i509.ziggurat.api.region.WorldRegion;
 import me.i509.ziggurat.internal.Uninstiantable;
-import me.i509.ziggurat.internal.ZigguratImplementation;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.PortalForcer;
 
 /**
  * An enumeration of all builtin flags.
  * @see FlagFactory
  */
 public final class Flags {
+	// TODO: Flag ideas
+	//  Block breaking
+	//  Block placing
+	//  Entity damaged? (May be hard)
+	//  Entity death? (May be hard)
+	//  Open container
+	//  Taking item out of item frame (Entity interaction?)
+	//  Rotating item frame (Entity interaction?)
+	//  Block interaction
+	//  Block attack
+	//  Use pressure plate
+	//  Decreasing hunger
+	//  Fall damage
+	//  Shoot projectile (Using item or entity for filtering?)
+
 	// SORTFIELDS:ON
+
+	/**
+	 * A flag which represents a boolean specifying whether explosions within a region can break blocks.
+	 */
+	public static final FlagType.Bool CAN_EXPLOSION_BREAK_BLOCKS = FlagFactory.ofBool(id("can_explosion_break_blocks"));
+
+	/**
+	 * A flag which represents a boolean specifying whether explosions within a region can damage entities.
+	 */
+	public static final FlagType.Bool CAN_EXPLOSION_DAMAGE_ENTITIES = FlagFactory.ofBool(id("can_explosion_damage_entities"));
 
 	/**
 	 * A flag which represents a boolean specifying whether fire can spread within a region.
 	 */
-	public static final FlagType.Bool CAN_FIRE_SPREAD = FlagFactory.ofBool(ZigguratImplementation.id("can_fire_spread"));
+	public static final FlagType.Bool CAN_FIRE_SPREAD = FlagFactory.ofBool(id("can_fire_spread"));
 
 	/**
 	 * A flag which represents a boolean specifying whether a nether portal can generate within a region.
+	 * If the flag value is false, nether portal creation will be cancelled or the {@link PortalForcer} will find a new position to spawn the portal at.
 	 */
-	public static final FlagType.Bool CAN_GENERATE_NETHER_PORTAL = FlagFactory.ofBool(ZigguratImplementation.id("can_generate_nether_portal"));
+	public static final FlagType.Bool CAN_GENERATE_NETHER_PORTAL = FlagFactory.ofBool(id("can_generate_nether_portal"));
 
 	/**
 	 * A flag which represents a set of all {@linkplain EntityType types of entities} that may spawn within a region.
 	 */
 	@SuppressWarnings({"unchecked", "rawtypes"}) // Thank you generics
-	public static final FlagType.Set<EntityType<?>> CAN_SPAWN_ENTITY = FlagFactory.ofRegistryEntrySet(ZigguratImplementation.id("can_spawn"), (Class<EntityType<?>>) (Class) EntityType.class, Registry.ENTITY_TYPE);
+	public static final FlagType.Set<EntityType<?>> CAN_SPAWN_ENTITY = FlagFactory.ofRegistryEntrySet(id("can_spawn_entity"), (Class<EntityType<?>>) (Class) EntityType.class, Registry.ENTITY_TYPE);
 
 	/**
 	 * A flag type which represents the {@linkplain UUID uuid} of the player which owns a region.
 	 * This flag is not applicable in any {@linkplain WorldRegion world} or {@linkplain GlobalRegion global} regions.
 	 */
-	public static final FlagType.Uuid OWNER = FlagFactory.ofUuid(ZigguratImplementation.id("owner"));
+	public static final FlagType.Uuid OWNER = FlagFactory.ofUuid(id("owner"));
 
 	// SORTFIELDS:OFF
 
